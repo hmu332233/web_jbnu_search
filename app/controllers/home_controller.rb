43e78ed1ee1d6cbced8993ffb9932c1022d74798
Your Name <you@example.com>
2016-09-11 13:15:02 +0000

    기본적인 파싱

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,21 @@ class HomeController < ApplicationController
 
     parser = JBNU_Parser.new
 
+    query = params[:query]
+    if query == nil
+      query = ""
+    end
 
-    @test = parser.start
+    @public = parser.start(0,query)
+    @notice = parser.start(1,query)
+    @recrult = parser.start(2,query)
+    @seminar = parser.start(3,query)
+  end
+
+  def search
+
+    @query = params[:query]
 
   end
+
 end
--- lib/JBNU_Parser.rb
+++ lib/JBNU_Parser.rb
@@ -1,19 +1,52 @@
+require 'Post'
 class JBNU_Parser
 
-  def start
-    uri = URI(URI.encode("http://www.chonbuk.ac.kr/bb/board.php?movePage=1&boardID=K_PUBLIC"))
+  # String[] boardId = {"K_PUBLIC","K_NOTICE","K_RECRUIT","K_SEMINAR","K_STUDY","K_ALBA","K_MARKET","K_LIVE","K_FOUND"};
+
+
+  def boardId(id)
+    boardId_t = ["K_PUBLIC","K_NOTICE","K_RECRUIT","K_SEMINAR"]
+    return boardId_t[id]
+  end
+
+  def start(id,word)
+    uri = URI(URI.encode("http://www.jbnu.ac.kr/bb/board.php?boardID="+boardId(id)+"&cTitle=Y&query="+word))
 
     req = Net::HTTP::Get.new(uri)
 
     res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') { |http| http.request(req) }
     doc = Nokogiri::HTML(res.body)
 
-    arr = doc.css(".listBoard//tbody//tr")
+    posts = []
 
-    return arr
+    doc.css(".programInfo//strong").each do |size_string|
+      unless size_string.inner_text == "0"
+
+
+        doc.css(".listBoard//tbody//tr").each do |post_data|
+
+          number = post_data.css("td")[0].inner_text
+          title = post_data.css("th")[0].inner_text
+          writer = post_data.css("td")[2].inner_text
+          period = post_data.css("td")[3].inner_text
+
+          content_link = post_data.css("th//a")[0].attr('href')
+
+          post = Post.new(number,title,content_link,period,writer)
+
+          posts << post
+        end
+
+      else
+        posts << Post.new("검색자료가없습니다","","","","")
+
+
+      end
+    end
+
+
+    return posts
 
   end
-
-
 
 end
