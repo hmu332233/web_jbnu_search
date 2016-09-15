4fcc7920e13ffb2e2e5426958f0d31f4567f9b77
Your Name <you@example.com>
2016-09-15 17:35:55 +0000

    테스트파일 삭제

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,21 @@ class HomeController < ApplicationController
   def main
     #test
     #test2
+
+    parser = JBNU_Parser.new
+
+    query = params[:query]
+    if query == nil
+      query = ""
+    end
+
+    @study = parser.getPosts(4,query)
+
+    if query == nil
+
+    else
+      #  redirect_to :anchor => 'service'
+    end
   end
 
   def index
@@ -15,13 +30,29 @@ class HomeController < ApplicationController
     if query == nil
       query = ""
     end
+ ["K_PUBLIC","K_NOTICE","K_RECRUIT","K_SEMINAR","K_STUDY","K_ALBA","K_MARKET","K_LIVE","K_FOUND"]
+    # @public = parser.getPosts(0,query)
+    # @notice = parser.getPosts(1,query)
+    # @recrult = parser.getPosts(2,query)
+    # @seminar = parser.getPosts(3,query)
+    @study = parser.getPosts(4,query)
+    # @alba = parser.getPosts(5,query)
+    # @market = parser.getPosts(6,query)
+    # @live = parser.getPosts(7,query)
+    # @found = parser.getPosts(8,query)
+    #
+    @public = @study
+    @notice = @study
+    @recrult = @study
+    @seminar = @study
 
-    @public = parser.getPosts(0,query)
-    #  @notice = parser.getPosts(1,query)
-     @recrult = parser.getPosts(4,query)
-    # @seminar = parser.start(4,query)
-    @notice = []
+    @alba = @study
+    @market = @study
+    @live = @study
+    @found = @study
+
     # @recrult = []
+
 
   end
 
