15a055b30398b8a386d6c66a71f4604b2e0d4867
Your Name <you@example.com>
2016-09-16 15:36:33 +0000

    프로토타입

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,23 +3,60 @@ require 'JBNU_Parser'
 class HomeController < ApplicationController
 
   def main
-    #test
-    #test2
+    @notice_list = %w[전체 학사공지 취업공지 일반공지 교내채용 특강/세미나]
+    @post_list =  %w[공모/스터디 아르바이트 판매/구매/광고 자취/하숙/원룸 분실/습득]
+
+    @query = params[:query]
+    @tab = params[:tab]
 
     parser = JBNU_Parser.new
 
-    query = params[:query]
-    if query == nil
-      query = ""
+    if @query == nil
+      @query = ""
     end
 
-    @study = parser.getPosts(4,query)
-
-    if query == nil
-
+    case @tab # a_variable is the variable we want to compare
+    when "전체"    #compare to 1
+      @boards = parser.getPosts(0,@query)
+      print "실행"
+    when "학사공지"    #compare to 2
+      @boards = parser.getPosts(0,@query)
+      print "실행"
+    when "취업공지"    #compare to 2
+      @boards = parser.getPosts(0,@query)
+      print "실행"
+    when "일반공지"    #compare to 2
+      @boards = parser.getPosts(1,@query)
+      print "실행"
+    when "교내채용"    #compare to 2
+      @boards = parser.getPosts(2,@query)
+      print "실행"
+    when "특강/세미나"    #compare to 2
+      @boards = parser.getPosts(3,@query)
+      print "실행"
+    when "공모/스터디"    #compare to 2
+      @boards = parser.getPosts(4,@query)
+      print "실행"
+    when "아르바이트"    #compare to 2
+      @boards = parser.getPosts(5,@query)
+      print "실행"
+    when "판매/구매/광고"    #compare to 2
+      @boards = parser.getPosts(6,@query)
+      print "실행"
+    when "자취/하숙/원룸"    #compare to 2
+      @boards = parser.getPosts(7,@query)
+      print "실행"
+    when "분실/습득"    #compare to 2
+      @boards = parser.getPosts(8,@query)
+      print "실행"
     else
-      #  redirect_to :anchor => 'service'
+      @boards = parser.getPosts(0,@query)
+      print "실행"
     end
+
+
+
+
   end
 
   def index
